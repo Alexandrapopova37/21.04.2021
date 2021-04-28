a38c4113ef061e007d77218779a35575bf502425
Гость <@b302-05.sttec.net>
2021-04-28 11:50:46 +0300

    first commit

---
+++ done.php
@@ -0,0 +1,15 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<h2>Сделайте выбор -></h2>
+<form action="form_reg.php" method="POST">
+<input type="submit" value="Регистрация">
+<h3></h3>
+<form action="form_login.php" method="POST">
+<input type="submit" value="Авторизация">
+</form>
+</body>
+</html>
